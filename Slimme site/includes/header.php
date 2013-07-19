5cfcbd0e6d413a8ea11fc353af6c320b371f4cba
Peter van der Zande <info@digitalbridge.nl>
2013-07-19 16:41:33 +0200

    header
    mijnspeurgroep css toegevoegd en wat links aangepast.

--- Slimme site/includes/header.php
+++ Slimme site/includes/header.php
@@ -1,3 +1,4 @@
+<?php session_start(); ?>
 <!DOCTYPE html>
 <!--[if IE 8]> 				 <html class="no-js lt-ie9" lang="en" > <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js" lang="en" > <!--<![endif]-->
@@ -9,13 +10,14 @@
   <link rel="stylesheet" href="../css/normalize.css" />
   <link href="../css/header.css" rel="stylesheet" type="text/css" />
   <link href="../css/contact.css" rel="stylesheet" type="text/css" />
+  <link href="../css/mijnspeurgroep.css" rel="stylesheet" type="text/css" />
   <link href="../css/login.css" rel="stylesheet" type="text/css" />
   <link href="../css/home.css" rel="stylesheet" type="text/css" />
   <link href="../css/helpeninfo.css" rel="stylesheet" type="text/css" />
   <link href="../css/foundation.css" rel="stylesheet">
   <!-- Included JS Files (Compressed) -->
   <script src="../js/foundation/jquery.js"></script>
-  <script src="../js/foundation/foundation.js"></script>
+  <script src="../js/vendor/custom.modernizr.js"></script>
   <script src="../js/json.js"></script>
 </head>
 
@@ -29,7 +31,7 @@
 <nav class="mainNav">
 		<ul>
         	<li><a href="index.php">Home</a></li>
-            <li><a href="#">Mijn Speurgroep</a></li>
+            <li><a href="mijnSpeurgroep.php">Mijn Speurgroep</a></li>
         	<li><a href="inloggen.php">Inloggen</a></li>
             <li><a href="contact.php">Contact</a></li>
         	<li><a href="helpNinfo.php?page=1">Help en Info</a></li>
