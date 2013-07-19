caf9914849c4ca3779f405022a99d831b3072e10
Peter van der Zande <info@digitalbridge.nl>
2013-07-19 16:42:06 +0200

    Login form aangepast conform foundation 4
    met error messages  JWZ!

--- Slimme site/inloggen.php
+++ Slimme site/inloggen.php
@@ -8,25 +8,28 @@ include('includes/header.php');
 </div>
 <div class="loginRight">
 	<p> Inloggen </p>
-	<form action="" method="post">
+	<form data-abide action="includes/verifyUser.php" method="post">
 		<table>
 			<tr>
 				<td>
-				Email adres:
+				Email adres: <small>required</small>
 				</td>
 				<td>
-				<input type=email name="email">
+				<input type="email" name="email" required>
+				<small class="error">Vul een e-mail adres in</small>
 				</td>
 			</tr>
 			<tr>
 				<td>
-				Wachtwoord:
+				Wachtwoord: <small>required</small>
 				</td>
 				<td>
-				<input type=password name="wachtwoord">
+				<input type="password" name="wachtwoord" required>
+				<small class="error">Wachtwoord is vereist</small>
 				</td>
 			</tr>
 		</table>
 		<input type=image alt="submit" src="images/inloggen.png" width="125" height="50">
+	</form>
 </div>      
 <?php include('includes/footer.php');?>
