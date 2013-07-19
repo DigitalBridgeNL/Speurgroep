b5d6b869ed235a88051b4e3331a5763b40f1f732
Peter van der Zande <info@digitalbridge.nl>
2013-07-19 13:53:11 +0200

    Css en dbinteraction + nav
    Navigatie aangepast met nieuwe links. Foundation overbodige shit
    weggehaald. DB interaction include toegevoegd.

--- Slimme site/includes/header.php
+++ Slimme site/includes/header.php
@@ -9,26 +9,28 @@
   <link rel="stylesheet" href="../css/normalize.css" />
   <link href="../css/header.css" rel="stylesheet" type="text/css" />
   <link href="../css/contact.css" rel="stylesheet" type="text/css" />
+  <link href="../css/login.css" rel="stylesheet" type="text/css" />
+  <link href="../css/home.css" rel="stylesheet" type="text/css" />
   <link href="../css/helpeninfo.css" rel="stylesheet" type="text/css" />
-  <link href="../css/foundation.min.css" rel="stylesheet">
+  <link href="../css/foundation.css" rel="stylesheet">
   <!-- Included JS Files (Compressed) -->
   <script src="../js/foundation/jquery.js"></script>
   <script src="../js/foundation/foundation.js"></script>
-  <script src="../js/foundation/foundation.placeholder.js"></script>
-  <script src="../js/foundation/foundation.reveal.js"></script>
   <script src="../js/json.js"></script>
 </head>
 
 <body>
+<?php include('includes/DBinteraction.php'); ?>
 <div class="center">
 <div class="logo">
 <img class="logo" src="../images/logo_2.png" width="200px" height="64px" />
 </div>
+
 <nav class="mainNav">
 		<ul>
-        	<li><a href="#">Home</a></li>
+        	<li><a href="index.php">Home</a></li>
             <li><a href="#">Mijn Speurgroep</a></li>
-        	<li><a href="#">Inloggen</a></li>
+        	<li><a href="inloggen.php">Inloggen</a></li>
             <li><a href="contact.php">Contact</a></li>
         	<li><a href="helpNinfo.php?page=1">Help en Info</a></li>
 		</ul>
