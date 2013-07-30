3febfa361843717d34eafb1640591333ddd8d9b9
Peter van der Zande <info@digitalbridge.nl>
2013-07-30 22:54:37 +0200

    json shit
    functies toegevoegd voorh et ophalen van mijnspeurgroep data

--- Slimme site/js/json.js
+++ Slimme site/js/json.js
@@ -54,3 +54,94 @@ function loadHTML(){
         //get <td> element values here!!??
     });
 };
+
+function loadBedrijf(){
+		var currentUser = document.getElementById('user').value;
+		$.ajax({
+		type: 'get',
+		url: 'includes/loadBedrijf.php', //in dit bestand staat een php variable dat de ID ophaalt. Met het ID kan een query uitgevoerd worden dat de content van de page ID ophaalt.
+		data: 'id='+currentUser,
+		success: function(data) {
+			var i, j, strHTML = ""; // data wordt weer opgehaald en geplaatst in de string.
+			for (i = 0; i < data.length; i += 1) {
+			strHTML += "<form><table>"
+			strHTML += "<tr><td>Contactpersoon</td><td><input type='text' name='contactpersoon' value='" + data[i]['contactpersoon'] + "' /></td></tr>"
+			strHTML += "<tr><td>Bedrijfsnaam</td><td><input type='text' name='bedrijfsnaam' value='" + data[i]['bedrijfsnaam'] + "' /></td></tr>"
+			strHTML += "<tr><td>Woon / Vestigingsadres</td><td><input type='text' name='adres' value='" + data[i]['adres'] + "' /></td></tr>"
+			strHTML += "<tr><td>Postcode</td><td><input type='text' name='postcode' value='" + data[i]['postcode'] + "' /></td></tr>"
+			strHTML += "<tr><td>Plaats</td><td><input type='text' name='plaats' value='" + data[i]['plaats'] + "' /></td></tr>"
+			strHTML += "<tr><td>Website</td><td><input type='text' name='website' value='" + data[i]['website'] + "' /></td></tr>"
+			strHTML += "<tr><td>E-Mailadres</td><td><input type='text' name='email' value='" + data[i]['email'] + "' /></td></tr>"
+			strHTML += "<tr><td>Telefoonnummer 1</td><td><input type='text' name='telefoonnummer1' value='" + data[i]['telefoonnummer1'] + "' /></td></tr>"
+			strHTML += "<tr><td>Telefoonnummer 2</td><td><input type='text' name='telefoonnummer2' value='" + data[i]['telefoonnummer2'] + "' /></td></tr>"
+			strHTML += "</table></form>"
+			}
+			$("#loadBedrijf").html(strHTML); // string wordt in het element met ID contentPage geplaatst.
+			}
+		});
+        //get <td> element values here!!??
+};
+
+function loadLogin(){
+		var currentUser = document.getElementById('user').value;
+		$.ajax({
+		type: 'get',
+		url: 'includes/loadLogin.php', //in dit bestand staat een php variable dat de ID ophaalt. Met het ID kan een query uitgevoerd worden dat de content van de page ID ophaalt.
+		data: 'id='+currentUser,
+		success: function(data) {
+			var i, j, strHTML = ""; // data wordt weer opgehaald en geplaatst in de string.
+			for (i = 0; i < data.length; i += 1) {
+			strHTML += "<form><table>"
+			strHTML += "<tr><td>Gebruikersnaam</td><td><input type='text' name='gebruikersnaam' value='" + data[i]['username'] + "' DISABLED /></td><td></td></tr>"
+			strHTML += "<tr><td>Wachtwoord</td><td><input type='password' name='wachtwoord' value='" + data[i]['wachtwoord'] + "' /></td><td><a href='#'>edit</a></td></tr>"
+			strHTML += "</table></form>"
+			}
+			$("#loadLogin").html(strHTML); // string wordt in het element met ID contentPage geplaatst.
+			}
+		});
+        //get <td> element values here!!??
+};
+
+function loadBranche(){
+		var currentUser = document.getElementById('user').value;
+		$.ajax({
+		type: 'get',
+		url: 'includes/loadBranche.php', //in dit bestand staat een php variable dat de ID ophaalt. Met het ID kan een query uitgevoerd worden dat de content van de page ID ophaalt.
+		data: 'id='+currentUser,
+		success: function(data) {
+			var i, j, strHTML = ""; // data wordt weer opgehaald en geplaatst in de string.
+			strHTML += "<table><tr><td>Hoofdgroep</td><td>" + data[0]['branche'] + "</td></tr>"
+			for (i = 1; i < data.length; i += 1) {
+			strHTML += "<tr><td>Subgroep " + i + "</td><td>" + data[i]['branche'] + "</td></tr>"
+			strHTML += "</table>"
+			}
+			$("#loadBranches").html(strHTML); // string wordt in het element met ID contentPage geplaatst.
+			}
+		});
+        //get <td> element values here!!??
+};
+
+function loadPakket(){
+		var currentUser = document.getElementById('user').value;
+		$.ajax({
+		type: 'get',
+		url: 'includes/loadPakket.php', //in dit bestand staat een php variable dat de ID ophaalt. Met het ID kan een query uitgevoerd worden dat de content van de page ID ophaalt.
+		data: 'id='+currentUser,
+		success: function(data) {
+			var i, j, strHTML = ""; // data wordt weer opgehaald en geplaatst in de string.
+			for (i = 0; i < data.length; i += 1) {
+			strHTML += "<table><tr><td>Pakket keuze:</td><td>" + data[i]['pakketid'] + "</td></tr>"
+			strHTML += "</table>"
+			}
+			$("#loadPakket").html(strHTML); // string wordt in het element met ID contentPage geplaatst.
+			}
+		});
+        //get <td> element values here!!??
+};
+
+function openModalwwvergeten() {
+	$( window ).load(function() {
+	
+	alert('gege');
+	});
+};
