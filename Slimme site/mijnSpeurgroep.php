f2e8255e5194606c4163be62b1c5d05155a0c6f2
Peter van der Zande <info@digitalbridge.nl>
2013-07-30 22:55:38 +0200

    mijn speurgroep id content tabs
    id aangepast zodat je met json die hsit in de juiste id kunt stoppen

--- Slimme site/mijnSpeurgroep.php
+++ Slimme site/mijnSpeurgroep.php
@@ -6,7 +6,19 @@ if ($_SESSION['myusername'] == '')
 }
 else {
 ?>
+
 <div class="clear"></div>
+<form>
+<input type="hidden" name="user" value="<?php echo $_SESSION['myusername'] ?>" id="user" />
+</form>
+<script>
+    $( window ).load(function() {
+        loadBedrijf();
+		loadLogin();
+		loadBranche();
+		loadPakket();
+    });
+</script>
 <div class="section-container tabs" data-section="tabs">
   <section>
     <p class="title" data-section-title><a href="#section1">Algemeen</a></p>
@@ -15,40 +27,73 @@ else {
       <div class="section-container auto" data-section>
         <section>
           <p class="title"><a href="#section1-1">Inloggegevens</a></p>
-          <div class="content">
+          <div class="content" id="loadLogin">
             <p>Login....</p>
           </div>
         </section>
         <section>
           <p class="title"><a href="#section1-2">Contract</a></p>
-          <div class="content">
+          <div class="content" id="loadContract">
             <p>Contract...</p>
           </div>
         </section>
 		<section>
           <p class="title"><a href="#section1-3">Gekozenbranches</a></p>
-          <div class="content">
+          <div class="content" id="loadBranches">
             <p>Gekozen branches...</p>
           </div>
         </section>
 		<section>
           <p class="title"><a href="#section1-4">Pakketkeuze</a></p>
-          <div class="content">
+          <div class="content" id="loadPakket">
             <p>Pakketkeuze...</p>
           </div>
         </section>
 		<section>
           <p class="title"><a href="#section1-5">Bedrijfsgegevens</a></p>
+          <div class="content" id="loadBedrijf">
+            <p>Bedrijfsgegevens...</p>
+          </div>
+        </section>
+    </div>
+	</div>
+  </section>
+  <section>
+    <p class="title" data-section-title><a href="#">Meldingen</a></p>
+    <div class="content" data-slug="section2" data-section-content>
+      <p>Content of section 2.</p>
+	  <div class="section-container auto" data-section>
+        <section>
+          <p class="title"><a href="#section2-1">Offertes</a></p>
+          <div class="content">
+            <p>Login....</p>
+          </div>
+        </section>
+        <section>
+          <p class="title"><a href="#section2-2">Mededelingen</a></p>
+          <div class="content">
+            <p>Contract...</p>
+          </div>
+        </section>
+		<section>
+          <p class="title"><a href="#section2-3">Nieuwsbrieven</a></p>
+          <div class="content">
+            <p>Gekozen branches...</p>
+          </div>
+        </section>
+		<section>
+          <p class="title"><a href="#section2-4">Pakketkeuze</a></p>
+          <div class="content">
+            <p>Pakketkeuze...</p>
+          </div>
+        </section>
+		<section>
+          <p class="title"><a href="#section2-5">Bedrijfsgegevens</a></p>
           <div class="content">
             <p>Bedrijfsgegevens...</p>
           </div>
         </section>
     </div>
-  </section>
-  <section>
-    <p class="title" data-section-title><a href="#">Section 2</a></p>
-    <div class="content" data-section-content>
-      <p>Content of section 2.</p>
     </div>
   </section>
 </div>
